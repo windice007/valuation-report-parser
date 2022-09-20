7b4e22be83fc454a38cb8b6e80ddcd82fc413a65
bing.feng <bing.feng@iquantex.com>
2022-09-20 19:52:39 +0800

    统一数据读取逻辑

--- excel/define.py
+++ excel/define.py
@@ -6,18 +6,9 @@ from base.utils import excel_column_index
 from model.mysql_models import BUSIPOSBOND, BUSIPOSFUTURE, BUSIPOSSTOCK, BUSIPOSREPO, BUSIPOSDEPOSIT, BUSIVALASSET
 
 
-class ProcessContext:
-    sheet: Sheet | None = None
-    row: int | None = None
-    subject_column: int = None
-
-    def __init__(self, sheet: Sheet) -> None:
-        self.sheet = sheet
-
-
 class DataCell:
-    address: str | int | None = None
-    capture_regex = None
+    address: str | None = None
+    capture_regex: str | None = None
 
     def __init__(self, address, regex=None):
         self.address = address
@@ -48,6 +39,18 @@ class ExcelConfig:
     pos_stock_define: List[PositionDefine] = None
 
 
+class ProcessContext:
+    sheet: Sheet | None = None
+    row: int | None = None
+    subject_column: int | None = None
+    config: ExcelConfig | None = None
+
+    def __init__(self, sheet: Sheet, config: ExcelConfig) -> None:
+        self.sheet = sheet
+        self.config = config
+        self.subject_column = excel_column_index(config.subject_code_column)
+
+
 class ValuationReportData:
     productCode: str | None = None
     productName: str | None = None
@@ -56,28 +59,15 @@ class ValuationReportData:
     summaries = []
 
 
-def capture_data(context: ProcessContext, cell: DataCell) -> str:
+def capture_data(sheet: Sheet, cell: DataCell, row: int = None) -> str:
     if cell == None or cell.address == None:
         return None
-    sheet = context.sheet
-    cell_value = sheet[cell.address]
-
-    if cell.capture_regex != None:
-        match = re.search(re.compile(cell.capture_regex), cell_value)
-        if match:
-            groups = match.groups()
-            if len(groups) > 0:
-                return match[1]
-            return match[0]
-    return cell_value
-
-
-def capture_line_data(context: ProcessContext, cell: DataCell, row: int) -> str:
-    if cell == None or cell.address == None:
-        return None
-    sheet = context.sheet
-    column_index = excel_column_index(cell.address)
-    cell_value = sheet.cell_value(row, column_index)
+    cell_value = None
+    if row is None:
+        cell_value = sheet[cell.address]
+    else:
+        column_index = excel_column_index(cell.address)
+        cell_value = sheet.cell_value(row, column_index)
 
     if cell.capture_regex != None:
         match = re.search(re.compile(cell.capture_regex), cell_value)
@@ -98,8 +88,9 @@ TYPE_MAPPING = {
 }
 
 
-def process_position(context: ProcessContext, config: ExcelConfig,  pos_type: str):
+def process_position(context: ProcessContext,  pos_type: str):
     sheet = context.sheet
+    config = context.config
     defines: List[PositionDefine] = getattr(config, pos_type)
     Model = TYPE_MAPPING[pos_type]
     data: dict = {}
@@ -123,29 +114,25 @@ def process_position(context: ProcessContext, config: ExcelConfig,  pos_type: st
                         obj = data[t_code]
 
                         for vd in sd.values:
-                            d = capture_line_data(context, vd.cell, i)
+                            d = capture_data(sheet, vd.cell, i)
                             setattr(obj, vd.column, d)
     return data
 
 
-def process_positions(context: ProcessContext, config: ExcelConfig, vpd: ValuationReportData):
-    d = process_position(context, config, "pos_bond_define")
+def process_positions(context: ProcessContext, vpd: ValuationReportData):
+    d = process_position(context, "pos_bond_define")
     for x in d.values():
         vpd.details.append(x)
 
 
-
 def process_excel_file_data(file, config: ExcelConfig) -> ValuationReportData:
-    sheet = p.get_sheet(
-        file_name=file)
-
-    context = ProcessContext(sheet)
-    context.subject_column = excel_column_index(config.subject_code_column)
+    sheet = p.get_sheet(file_name=file)
+    context = ProcessContext(sheet, config)
 
     vpd = ValuationReportData()
-    vpd.productCode = capture_data(context, config.product_code)
-    vpd.valuationDate = capture_data(context, config.valuation_date)
+    vpd.productCode = capture_data(context.sheet, config.product_code)
+    vpd.valuationDate = capture_data(context.sheet, config.valuation_date)
 
-    process_positions(context, config, vpd)
+    process_positions(context, vpd)
 
     return vpd
