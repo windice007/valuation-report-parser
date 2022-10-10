c16d066fa2afbf9c8c99ec037ad03d4b45e477c3
bing.feng <bing.feng@iquantex.com>
2022-10-10 14:26:37 +0800

    批量解析当前目录文件

--- run.py
+++ run.py
@@ -3,14 +3,28 @@ import json
 from base.db_mysql import obj_json_default
 from excel import process_excel_file_data as process
 from excel.utils import obj_json_hook
+import pyexcel_xls
+import pyexcel_xlsx
+import pyexcel_io.writers
+import os
+import glob
+
+
+def current_dir_files():
+    return glob.glob("*.xls*")
 
 
 if __name__ == "__main__":
-
-    file = "d:/纯固收产品2估值表2021-07-02.xls"
     with open('config.json', 'r', encoding="utf-8") as f:
         config = json.load(f, object_hook=obj_json_hook)
-        vpd = process(file, config)
-        with open('result.json', 'w', encoding="utf-8") as writer:
-            json.dump({"positions": list(vpd.details.values()), "product": vpd.product}, writer,  default=obj_json_default,
-                      indent=2, ensure_ascii=False)
+
+        files = current_dir_files()
+
+        if len(files) == 0:
+            print("当前目录没有找到估值文件(*.xls|*.xlsx)")
+
+        for file in current_dir_files():
+            vpd = process(file, config)
+            with open(f'{file}.json', 'w', encoding="utf-8") as writer:
+                json.dump({"positions": list(vpd.details.values()), "product": vpd.product}, writer,  default=obj_json_default,
+                          indent=2, ensure_ascii=False)
