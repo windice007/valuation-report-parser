861859a8f1ff863d379fcab9d0b0709ca3029db3
bing.feng <bing.feng@iquantex.com>
2023-08-16 16:41:26 +0800

    完善config_template #0

--- vrp/entry/config_template.py
+++ vrp/entry/config_template.py
@@ -12,6 +12,8 @@ from vrp.excel.sink import check_db_settings, obj_json_default
 from vrp.excel.utils import Dict
 import json
 from sqlalchemy import Table, Column
+import datetime
+import decimal
 
 
 class Args(Protocol):
@@ -34,8 +36,19 @@ def table_dict(table: Table):
 
     for v in table.columns:
         col: Column = v
-        result[col.key] = ""
-
+        col_type = col.type.python_type
+        if issubclass(col_type, datetime.date):
+            result[col.key] = datetime.datetime.now().strftime("%Y-%m-%d")
+        elif (
+            issubclass(col_type, decimal.Decimal)
+            or issubclass(col_type, float)
+            or issubclass(col_type, int)
+        ):
+            result[col.key] = 0
+        elif issubclass(col_type, str):
+            result[col.key] = ""
+        else:
+            result[col.key] = None
     return result
 
 
