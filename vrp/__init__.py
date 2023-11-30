406172df3f7647968701562dca13230c915d690a
bing.feng <bing.feng@iquantex.com>
2023-11-30 11:10:44 +0800

    add hidden import

--- vrp/__init__.py
+++ vrp/__init__.py
@@ -1,7 +1,7 @@
 """
 valuation report parser
 """
-__version__ = "0.2.17"
+__version__ = "0.2.17.1"
 
 
 from typing import Protocol
--- vrp/run.py
+++ vrp/run.py
@@ -13,6 +13,7 @@ from vrp.excel.utils import Dict, obj_json_hook
 # hidden import
 import pyexcel_xls
 import pyexcel_xlsx
+import pyexcel_io.readers
 import pyexcel_io.writers
 from cryptography.hazmat.primitives.kdf import pbkdf2
 import opengauss_sqlalchemy.psycopg2
