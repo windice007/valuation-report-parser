34183361b98bbb538a162d63c28f68ab6e796bc7
bing.feng <bing.feng@iquantex.com>
2024-11-06 18:47:09 +0800

    增加必要的注释

--- vrp/excel/define.py
+++ vrp/excel/define.py
@@ -5,17 +5,48 @@ MERGED_VALUE_TYPE = Literal["up", "down", "left", "right", "auto"]
 
 
 class DataCell(Protocol):
+    """
+    属性数据配置
+    """
+
     address: str
+    """
+    address: 地址配置，针对Excel，可以配置为绝对地址（行和列都配置，如'A3'）和相对地址(只配置列，如'A')，相对地址只能用在position配置中
+    """
     capture_regex: str
+    """
+    capture_regex: 获取数据的正则表达式，如果不设置就是原始数据。
+    """
     subject_code: str
+    """
+    subject_code: 基于科目代码来定位和获取数据，相当于间接设置行
+    """
     mapping: dict
+    """
+    mapping: 数据转换规则配置
+    """
     mapping_rule: Literal["contains", "equals", "regex", None]
+    """
+    mapping_rule: 数据转换规则模式的配置，默认为equals
+    可以设置的选项为: ["contains", "equals", "regex"]
+    """
     formula: str
+    """
+    formula: 通过表达式来返回数据
+    """
     type: Target_Type | None
+    """
+    type: 数据的类型设置，如果配置了数据库连接，引擎会自动根据数据库字段类型来进行判断，无需设置。
+    可以设置的选项为: ["number", "str", "date", "datetime"]
+    """
     subject_filter_regex: str | None
     filter_formula: str | None
     value: Any
     merged_value: MERGED_VALUE_TYPE | None
+    """
+    merged_value: 数据如果为空，可以设置进行单元格数据合并，自动获取上下左右第一个部位空的数据作为有效数据返回。
+    可以设置的选项为: ["up", "down", "left", "right", "auto"]
+    """
 
 
 class HandlerDefine(Protocol):
